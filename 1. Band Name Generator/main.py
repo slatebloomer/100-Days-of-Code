70402375043e8c934c97a47ed8c0fadf15709039
Nicola Slater-Arnold <nicolaslaterarnold@gmail.com>
2023-01-10 20:00:45 +0000

    initial commit

---
+++ 1. Band Name Generator/main.py
@@ -0,0 +1,8 @@
+# This is a very simple program designed to test my understanding of print statements and inputs.
+
+print("Hello and welcome to the band name generator!")
+
+city = input("Which city did you grow up in?\n")
+pet = input("What's the name of your first pet?\n")
+
+print(f"Please welcome to the stage {city} {pet}!")
